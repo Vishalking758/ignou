0b0523a44d2ccd6680efa5eb1f430944c5514071
Vishal Sharma <97242441+Vishalking758@users.noreply.github.com>
2023-01-25 01:50:05 -0800

    Update jsFile.js
    mouse slider update

--- assets/ownStyle/jsFile.js
+++ assets/ownStyle/jsFile.js
@@ -25,3 +25,11 @@ $(document).ready(function () {
 
 
 })
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
